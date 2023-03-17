06b917409de3cbcc6274fcd05bb64ea44a87af0f
spooseh <spooseh@gmail.com>
2023-03-17 11:51:41 +0100

    Improvements

--- ols.jl
+++ ols.jl
@@ -1,24 +1,19 @@
-using Random
+using Random, DataFrames, StatsModels, GLM
+
 Random.seed!(1984)
 n = 100
 
 x = 2.5 * randn(n) .+ 1.5   # Array of n values with mean = 1.5, stddev = 2.5
 res = 0.5 * randn(n)        # Generate n residual terms
-y = 2 .+ 0.3 * x .+ res          # Actual values of Y
+y = 2 .+ 0.3 * x .+ res     # Actual values of Y
 
-using Plots
-scatter(x,y)
-
-using DataFrames
 data = DataFrame(X=x, Y=y)
 
-using StatsModels
 fm = @formula(Y ~ X)
 
-using GLM
-mod = lm(fm, data)
+fit_ols = lm(fm, data)
 
-println(mod)
+println(fit_osl)
 
 # julia --project=./ ols.jl from Terminal
 # include("ols.jl") from REPL
